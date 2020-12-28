7c824344968dfe2f82e8d1bb6db587de61b2f1f0
yscman <cvg.goncalves@protonmail.com>
2020-12-28 17:36:23 -0500

    fixing disqus

--- pelicanconf.py
+++ pelicanconf.py
@@ -75,7 +75,7 @@ COPYRIGHT_YEAR = datetime.now().year
 DEFAULT_PAGINATION = 10
 
 DISQUS_SITENAME = "data log"
-ADD_THIS_ID = "disqus_XSVEjw9R5B"
+ADD_THIS_ID = "@disqus_XSVEjw9R5B"
 
 STATIC_PATHS = ["images", "extra/ads.txt", "extra/CNAME"]
 
